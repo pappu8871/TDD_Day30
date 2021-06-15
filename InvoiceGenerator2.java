d0179d714e8be329c84c252c1d52d3ab4312b695
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-15 11:00:24 +0530

    Added: Min fare

---
+++ InvoiceGenerator2.java
@@ -0,0 +1,13 @@
+package com.invoiceGenerator;
+
+public class InvoiceGenerator2 {
+	private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
+    public double calculateMINFare(double distance, int time) {
+    	 double totalFare= distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+		if(totalFare < MINIMUM_FARE)
+		return MINIMUM_FARE;
+		return totalFare;
+    }
+}
