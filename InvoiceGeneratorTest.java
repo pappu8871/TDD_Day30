08facd372192fc8afc1b0a3e4892404ed0cf3575
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-15 11:41:24 +0530

    Added:Test Cases

---
+++ InvoiceGeneratorTest.java
@@ -0,0 +1,36 @@
+package com.invoiceGenerator;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class InvoiceGeneratorTest {
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator =new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinFare() {
+		InvoiceGenerator2 invoiceGenerator =new InvoiceGenerator2();
+		double distance = 0.1;
+		int time = 1;
+		double totalfare = invoiceGenerator.calculateMINFare(distance, time);
+		Assert.assertEquals(5.0, totalfare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator =new InvoiceGenerator ();
+		Ride[] rides={ new Ride(2.0, 5),
+				new Ride(0.1, 1)
+		};
+		double fare= InvoiceGenerator.calculateFare(rides);		
+		Assert.assertEquals(30, fare,0.0);
+	}
+
+}
